bad19585a41a6ae5b753eb89a245af6be926bbce
Ken-muigai <kenmuigai980@gmail.com>
2021-03-27 19:48:51 +0300

    moved data to environment

--- employeemanager/src/app/employee.service.ts
+++ employeemanager/src/app/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
