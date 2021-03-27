87e3449eab334a38e77c87f6a82a1ce5581821c3
Ken-muigai <kenmuigai980@gmail.com>
2021-03-27 17:47:52 +0300

    get and post methods added

--- employeemanager/src/app/employee.service.ts
+++ employeemanager/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,20 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  getEmployees(): Observable<any>{
-    return this.http.get<any>(`${this.apiService}/employee/all`)
+  //Gets
+  getEmployees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(`${this.apiService}/employee/all`)
   }
 
-  getEmployeeById(id:Number){
+  getEmployeeById(id:number){
     return this.http.get(`${this.apiService}/find/${id}`)
+  }
+
+
+  //Posts
+  createEmployee(employee: Employee){
+    return this.http.post(`${this.apiService}/add`, employee)
+    .subscribe(() => { console.log(employee) })
   }
 
 
