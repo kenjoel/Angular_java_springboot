31e5482fcdc7553c3db8e6b461f01850c49e62e3
Ken-muigai <kenmuigai980@gmail.com>
2021-03-30 12:02:24 +0300

    added modal

--- employeemanager/src/app/app.component.ts
+++ employeemanager/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { error } from 'protractor';
+import { Button, error } from 'protractor';
 import { Employee } from './employee';
 import { EmployeeService } from "./employee.service";
 
@@ -11,18 +11,39 @@ import { EmployeeService } from "./employee.service";
 })
 export class AppComponent implements OnInit {
 
-  public employee: Employee[] = [];
+  public employees: Employee[] = [];
 
   constructor(private service: EmployeeService){}
 
   public getEmployees(){
     this.service.getEmployees().subscribe((response: Employee[]) => {
-      this.employee = response
+      this.employees = response
     },
     (err: HttpErrorResponse) => {
       alert(err.message)
     }
     )
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void{
+    const button = document.createElement("button")
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-toggle", "modal");
+
+    if(mode === "add"){
+      button.setAttribute("data-target", "#addEmployeeModal")
+    }
+
+    if(mode === "edit"){
+      button.setAttribute("data-target", "#updateEmployeeModal")
+    }
+
+
+    if(mode === "delete"){
+      button.setAttribute("data-target", "#deleteEmployeeModal")
+    }
+   
   }
 
 
--- employeemanager/src/app/employee.ts
+++ employeemanager/src/app/employee.ts
@@ -3,7 +3,7 @@ export interface Employee {
     name:string,
     email:string,
     jobTitle:string,
-    phone:number,
+    phoneNumber:number,
     imageUrl:string,
     employeeCode:number
 }
